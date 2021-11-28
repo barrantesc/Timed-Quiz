6c3acf0119b779c7733dc09c2f5b15162097215c
playground <connie.s.barrantes@gmail.com>
2021-11-28 00:50:52 -0500

    Made question section with javascript and HTML

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Timed-Quiz
+
+## Purpose
+A times quiz application with multiple choice questions on Javascript fundamentals that stores high scores for users to measure progress compared to other peers. 
+
+## Built with
+*HTML
+*CSS
+*Javascript
+
+## Minimun Requirements
+
+
+
+![Mockup of final product]() 
+
+## Website
+
+
+## Contribution
+Made with ❤️ by Connie Barrantes
---
+++ assets/js/questions.js
@@ -0,0 +1,33 @@
+const questions = [
+    { 
+        question: "Inside which HTML element do we put the javaScript?",
+        choices:
+        ["<scripting>", "<script>", "<js>", "<javascript>"],
+        answer: "<script>"
+    },
+    {
+        question: "How do you write 'Hello World' in an alert box?",
+        choices: ['msg("Hello World");', 'alert("Hello World");', 'msgBox("Hello World");', 'alertBox("Hello World");'],
+        answer: 'alert("Hello World");'
+    },
+    {
+        question: "How to write an IF statement in JavaScript?",
+        choices: ["if i=5 then", "if(i==5", "if i==5 then", "if i=5"],
+        answer: "if(i==5)"
+    },
+    {
+        question: "How does a FOR loop start",
+        choices: ["for (i <= 5; i++)", "for (i=0; i<=5; i++)", "for (i = 0; i <= 5)", "for i=1 to 5"],
+        answer:  "for (i = 0; i <= 5; i++"
+    },
+    {
+        question: "Which operator is used to assign a value to a variable?",
+        choices: ["X", "*", "-", "="],
+        answer: "="
+    },
+    {
+        question: "Which event occurs when the user clicks on an HTML element?",
+        choices: ["onmouseover", "onclick", "onmouseclick", "onchange"],
+        answer: "onclick"
+    },
+]
