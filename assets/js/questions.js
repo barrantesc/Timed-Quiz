ad2b1aa6950d67a4f289779f000363617d217334
playground <connie.s.barrantes@gmail.com>
2021-11-28 16:41:49 -0500

    created a submit score function, final score function , gameover function, edit some Ids and CSS

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -2,32 +2,34 @@ const questions = [
     { 
         question: "Inside which HTML element do we put the javaScript?",
         choices:
-        ["<scripting>", "<script>", "<js>", "<javascript>"],
-        answer: "<script>"
+        ["scripting", "script", "js", "javascript"],
+        answer: "b"
     },
     {
         question: "How do you write 'Hello World' in an alert box?",
         choices: ['msg("Hello World");', 'alert("Hello World");', 'msgBox("Hello World");', 'alertBox("Hello World");'],
-        answer: 'alert("Hello World");'
+        answer: 'b'
     },
     {
         question: "How to write an IF statement in JavaScript?",
-        choices: ["if i=5 then", "if(i==5", "if i==5 then", "if i=5"],
-        answer: "if(i==5)"
+        choices: ["if i=5 then", "if(i==5)", "if i==5 then", "if i=5"],
+        answer: "b"
     },
     {
         question: "How does a FOR loop start",
         choices: ["for (i <= 5; i++)", "for (i=0; i<=5; i++)", "for (i = 0; i <= 5)", "for i=1 to 5"],
-        answer:  "for (i = 0; i <= 5; i++"
+        answer:  "b"
     },
     {
         question: "Which operator is used to assign a value to a variable?",
         choices: ["X", "*", "-", "="],
-        answer: "="
+        answer: "d"
+
     },
     {
         question: "Which event occurs when the user clicks on an HTML element?",
         choices: ["onmouseover", "onclick", "onmouseclick", "onchange"],
-        answer: "onclick"
+        answer: "b"
     },
-]
+
+];
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,108 @@
-// Gather HTML elements for manipulation
-var highScores = document.getElementById("scores");
-var timer = document.getElementById("timer");
+// Declaring Element
 var startDiv = document.getElementById("start");
+var questionSectionDiv = document.getElementById("question-section");
 var questionDiv = document.getElementById("questions");
-var answerSection = document.getElementById("answer-buttons");
-var endOfGame = document.getElementById("gameover");
-var finalScore = document.getElementById("final-score");
+var button1 = document.getElementById("btn1");
+var button2 = document.getElementById("btn2");
+var button3 = document.getElementById("btn3");
+var button4 = document.getElementById("btn4");
+var endOfGame = document.getElementById("gameOver");
+var final= document.getElementById("final-score");
 var playerInitials = document.getElementById("initials");
-var submitButton = document.getElementById("submit");
 var highScoreContainer = document.getElementById("highScoreContainer");
-var highScoreDiv = document.getElementById("highscore");
-var highScorePage = document.getElementById("highscorepage");
-var highScoreHeader = document.getElementById("highScoreHeader");
-var highScoreInitials = document.getElementById("highscore-initials");
-var highScores = document.getElementById("highscores");
-var engGameBtn = document.getElementById("End-Btns");
-var playAgainBtn = document.getElementById("play-again");
-var clearScoresBtn = document.getElementById("clearScores");
+var highScoreList = document.getElementById("highScoreList");
+
+
+
 
 var finalQuestionIndex = questions.length;
 var currentQuestionIndex = 0;
 var timeLeft = 50;
-var timerInterval;
-var score = 0
-var correct;
+var score = 0;
+var initials;
+// var timerInterval;
 
-function replayQuiz(){
-    highScoreContainer.style.display = "none";
-    
+
+// Start Quiz Button Function
+function startQuiz(){
+    console.log(question);
+    startDiv.style.display = "none";
+    questionSectionDiv.style.display = "block"
+    populateQuestions();
 }
 
-function clearScore (){
-    window.localStorage.clear();
-   highscoreDisplayName.textContent = "";
-   highscoreDisplayScore.textContent = ""; 
+// Populating questions along with answers
+function populateQuestions(){
+    if (currentQuestionIndex === finalQuestionIndex) {
+        showGameOver();
+    }
+   question.innerHTML = questions[currentQuestionIndex].question;
+    // console.log(questions[currentQuestionIndex].question)
+
+   button1.innerHTML = questions[currentQuestionIndex].choices[0]
+//    console.log("testing",questions[currentQuestionIndex].choices[0])
+
+   button2.innerHTML = questions[currentQuestionIndex].choices[1]
+
+   button3.innerHTML = questions[currentQuestionIndex].choices[2]
+
+   button4.innerHTML = questions[currentQuestionIndex].choices[3]
+
 }
+// Function checks the response of the answer
+function checkAnswer(answer) {
+    var correctAnswer = questions[currentQuestionIndex].answer
+    // console.log(correctAnswer)
+
+
+    if (answer === correctAnswer && currentQuestionIndex !== finalQuestionIndex){
+        // console.log("correctAnswer")
+        score++;
+        currentQuestionIndex++;
+        populateQuestions();
+
+    }else if (answer !== correctAnswer & currentQuestionIndex !== finalQuestionIndex) {
+        // console.log("incorrecAnswer")
+        currentQuestionIndex++;
+        populateQuestions();
+
+    }else {
+    // console.log("finished");
+    showGameOver();
+    console.log(highScoreContainer)
+    }
+}
+
+function showGameOver() {
+    endOfGame.style.display = "block";
+    questionSectionDiv.style.display = "none";
+    finalScore();
+}
+
+function finalScore() {
+    final.innerHTML = score;
+}
+
+function submitScore() {
+    initials = playerInitials.value
+    console.log(initials);
+    window.localStorage.setItem(initials,score);
+
+    endOfGame.style.display = "none"
+    highScoreContainer.style.display = "block";
+    showHighScores();
+}
+
+function showHighScores() {
+    var li = document.createElement("li");
+     var text = document.createTextNode(`${initials}-${score}`);
+     li.appendChild(text);
+     highScoreList.appendChild(li);
+}
+
+
+// function clearScore (){
+//     window.localStorage.clear();
+//    highscoreDisplayName.textContent = "";
+//    highscoreDisplayScore.textContent = ""; 
+// }
